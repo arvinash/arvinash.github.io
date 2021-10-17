332c3dd6e77a6bf381166397e130b02bd18bd37f
Arvinashvell <arvinashv@gmail.com>
2021-10-18 04:45:16 +0800

    question4

--- assignment4/js/question4.js
+++ assignment4/js/question4.js
@@ -1,22 +1,13 @@
-function validatePalin(str) {
-
-    // get the total length of the words
-    const len = string.length;
-
-    // Use for loop to divide the words into 2 half
-    for (let i = 0; i < len / 2; i++) {
-
-        // validate the first and last characters are same
-        if (string[i] !== string[len - 1 - i]) {
-            alert( 'It is not a palindrome');
-        }
-    }
-    alert( 'It is a palindrome');
-}
-
-// accept the string or number from the prompt
-const string = prompt('Enter a string or number: ');
-
-const value = validatePalin(string);
-
-console.log(value);
+function palindrome()
+		{
+		var a= document.getElementById("pa").value;
+		var b = "";
+		for (i = a.length-1; i >= 0; i--)
+		{
+		b = b + a[i]
+		}
+		if (a == b)
+		document.getElementById("pa2"). value = b + " is a Palindrome String ";
+		else
+		document.getElementById("pa2"). value = b + " is not a Palindrome String";
+		}
